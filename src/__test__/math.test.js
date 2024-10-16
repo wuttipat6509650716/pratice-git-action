8cb3aba179c65384bfe8eb19641aa4c07cd72e82
EC2 Default User <ec2-user@ip-172-31-19-106.ec2.internal>
2024-10-16 06:57:54 +0000

    Test Add Subtract Multiply

---
+++ src/__test__/math.test.js
@@ -0,0 +1,23 @@
+const { add, subtract, multiply } = require('../math.js');
+
+describe('simple arithmetic', () => {
+describe('addition', () => {
+it('should return the sum of two numbers', async ()  =>{
+expect(add(1,2)).toBe(3);
+});
+});
+
+describe('subtract', () => {
+it('should return the difference of two numbers', async ()  =>{
+expect(subtract(10,2)).toBe(8);
+});
+});
+
+describe('multiply', () => {
+it('should return multiply of two numbers', async ()  =>{
+expect(multiply(5,2)).toBe(10);
+});
+});
+
+});
+
